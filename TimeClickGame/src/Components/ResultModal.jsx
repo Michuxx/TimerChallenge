29d30cd868e4d2d42282bc9156ce4945a95eb6f2
Michuxx <104158118+Michuxx@users.noreply.github.com>
2024-06-17 18:48:52 +0200

    Result fixed

--- TimeClickGame/src/Components/ResultModal.jsx
+++ TimeClickGame/src/Components/ResultModal.jsx
@@ -1,10 +1,13 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { remainingTime, targetTime, onReset },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -14,14 +17,15 @@ const ResultModal = forwardRef(function ResultModal(
   });
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result} </h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         the target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form action="dialog">
+      <form action="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
